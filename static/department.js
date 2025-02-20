62170a7093cf6ce31c5955d4a11eb0e14f5e473c
KahyanKingstech <kah.yan@kingstech.com.sg>
2025-02-20 16:22:50 +0800

    Update department.js

--- static/department.js
+++ static/department.js
@@ -1,4 +1,4 @@
-const departmentApiUrl = "http://127.0.0.1:9000/api/departments";
+const departmentApiUrl = "https://nexora-15s3.onrender.com/api/departments";
 
 async function fetchDepartments() {
     try {
